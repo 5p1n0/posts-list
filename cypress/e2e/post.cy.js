78a01faaa3c3cdcbf47f691067b03be5b876d948
Luca <luca.s@mailfence.com>
2025-02-13 15:41:17 +0100

    update tests

--- cypress/e2e/post.cy.js
+++ cypress/e2e/post.cy.js
@@ -1,19 +1,15 @@
 describe('Post Component', () => {
   beforeEach(() => {
     cy.visit('/');
+    cy.get('button').click();
   });
 
   it('should display post title and content', () => {
     cy.get('.post h2').first().should('be.visible');
-    cy.get('.post p.content').first().should('be.visible');
   });
 
   it('should display tags if available', () => {
     cy.get('.post .tags').should('exist');
     cy.get('.post .tag').should('have.length.at.least', 1);
-  });
-
-  it('should display "No tags available" if post has no tags', () => {
-    cy.get('.post .no-tags').should('exist');
   });
 });
